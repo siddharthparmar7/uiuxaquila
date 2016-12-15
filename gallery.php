1ae419c8fe711906369b2f83536ba14aac29d1fb
Sid Parmar <Sid@Sids-MacBook-Pro.local>
2016-12-15 12:18:45 -0800

    made few changes for responsiveness, sending it to Steve for help and feedback

--- gallery.php
+++ gallery.php
@@ -81,11 +81,11 @@ Date : 7-11-16
 
     <!-- Left and right controls -->
     <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true" style="color: #FF4081; font-size:70px;"></span>
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true" style=""></span>
       <span class="sr-only">Previous</span>
     </a>
     <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true" style="color: #FF4081; font-size: 70px;"></span>
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true" style=""></span>
       <span class="sr-only">Next</span>
     </a>
   </div>
